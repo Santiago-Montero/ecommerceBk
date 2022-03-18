ec87e9dad7b8b5b6cae5b3fcce8dcd4c656828da
santimontero <smontero@frba.utn.edu.ar>
2022-03-18 17:52:43 -0300

    cambiando DB_URL

--- contenedores/config.js
+++ contenedores/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongodb: {
-        url: process.env.DB_URL || "mongodb+srv://santi:santi@cluster0.j0w00.mongodb.net/ecommerce?retryWrites=true&w=majority",
+        url: process.env.DB_URL,
         options: {
             useNewUrlParser: true,
             useUnifiedTopology: true,
